97fcf8ac075abaf74c513d9a60d5f681c57bb3ea
Pachu03 <franfervaz03@gamil.com>
2024-05-22 17:36:45 +0200

    login_ creacion de roles y permisos

---
+++ database/migrations/2024_05_21_153245_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_collection');
+            $table->integer('quantity_collection');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+            $table->dropForeign(['group_id']);
+        });
+        Schema::dropIfExists('collections');
+    }
+};
---
+++ database/migrations/2024_05_21_153246_create_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('type_report');
+            $table->date('data_report');
+            $table->foreignId('collection_id')->constrained('collections')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropForeign(['collection_id']);
+        });
+        Schema::dropIfExists('reports');
+    }
+};
