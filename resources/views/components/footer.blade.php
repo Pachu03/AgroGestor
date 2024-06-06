4a5470a3fe6bdd0b977ccacd0ed5b1d465f6b3b0
Practicas1 <practicas1@grupocarlosalcaraz.com>
2024-06-06 14:21:42 +0200

    optimizar_codigo actualizar el footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,27 @@
 <style>
-    .content-wrapper {
-        flex: 1;
-    }
-
     body {
         display: flex;
         flex-direction: column;
         min-height: 100vh;
+        margin: 0;
+
     }
 
+    .content-wrapper {
+        flex: 1;
+    }
+
+    /* Estilo para el footer */
     footer {
         text-align: center;
         padding: 1rem;
         background: #f8f9fa;
-        position: relative;
-        bottom: 0;
         width: 100%;
     }
 </style>
 
 <footer class="footer mt-auto py-3 bg-white">
     <div class="container">
-        <span class="text-muted">&copy; {{ date('Y') }} Agrogestor. @lang('All rights reserved').</span>
+        <span class="text-muted">&copy; 2024 Agrogestor. Todos los derechos reservados.</span>
     </div>
 </footer>
